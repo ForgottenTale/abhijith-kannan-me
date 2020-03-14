51d566ad57adb0944dc96360c0576c3555651878
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-03-14 12:58:37 +0530

    first commit

---
+++ precache-manifest.282a628c770b88db0d5920c4a8704eab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f087b30a73184c38e746995acdb4cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "034e24261565ac179f8d",
+    "url": "/static/css/main.faf65e10.chunk.css"
+  },
+  {
+    "revision": "c4b2fe00c172d16bcb2d",
+    "url": "/static/js/2.39992bb1.chunk.js"
+  },
+  {
+    "revision": "034e24261565ac179f8d",
+    "url": "/static/js/main.b2457be3.chunk.js"
+  },
+  {
+    "revision": "41cb584af1f9b9ac9488",
+    "url": "/static/js/runtime-main.ad17772d.js"
+  },
+  {
+    "revision": "ecf3499f24e73831d479f77f73a5ebb7",
+    "url": "/static/media/photo.ecf3499f.webp"
+  }
+]);
